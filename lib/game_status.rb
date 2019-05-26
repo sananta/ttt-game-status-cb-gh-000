a0986a471ad852f783522d6273f3c3493af7e9ce
Varshini Ananta <sananta@ucdavis.edu>
2019-05-26 22:27:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+
+  all_empty = board.all? {|x| x == " "}
+  if all_empty == true
+    return false
+  end
+
+  winningCombo = WIN_COMBINATIONS.detect do |combo|
+    combo.all? {|x| board[x] == "X"} || combo.all? {|x| board[x] == "O"}
+  end
+
+end
+
+def full?(board)
+  !board.any?{|i| i == " "}
+end
